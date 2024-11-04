f12488b52fbbeaf357b89853e1127903573b68c2
UDsGitHub <dingding8003@gmail.com>
2024-11-03 19:30:43 -0600

    Added functions for handling friends objects and dm chatting

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,26 @@
 import express from "express";
-import { getUser, getDmUsers, sendMessageToUser } from "../controllers/user.js";
+import {
+  getUser,
+  getDmUsers,
+  getFriends,
+  getFriendRequests,
+  sendMessageToUser,
+  sendFriendRequest,
+  ignoreFriendRequest,
+  addFriend,
+  unFriend,
+} from "../controllers/user.js";
+import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
 
-router.get("/getUser", getUser);
-router.get("/getDmUsers/:userId", getDmUsers);
-router.post("/sendMessageToUser", sendMessageToUser);
+router.get("/getUser", verifyToken, getUser);
+router.get("/getDmUsers", verifyToken, getDmUsers);
+router.get("/getFriends", verifyToken, getFriends);
+router.get("/getFriendRequests", verifyToken, getFriendRequests);
+router.post("/sendMessageToUser", verifyToken, sendMessageToUser);
+router.post("/sendFriendRequest", verifyToken, sendFriendRequest);
+router.delete("/ignoreFriendRequest", verifyToken, ignoreFriendRequest);
+router.post("/addFriend", verifyToken, addFriend);
+router.delete("/unFriend", verifyToken, unFriend);
 
 export default router;
