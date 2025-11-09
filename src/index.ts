bd05832ff73c0caa84454c0a13f0b0f141e26c98
UDsGitHub <dingding8003@gmail.com>
2025-11-09 00:34:37 -0600

    adding home route for testing

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,13 @@ import serverRoutes from "./routes/serverRoutes.js";
 import { verifyToken, errorHandler } from "./lib/middleware/index.js";
 
 const app = express();
-console.log('testing 12')
+console.log("testing 12");
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({ origin: config.corsOrigin, credentials: true }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => res.json({ message: "hello world" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/user", verifyToken, userRoutes);
 app.use("/api/server", verifyToken, serverRoutes);
