2da005c88d55a0eb449f24eed53a3f1a87afcb7f
UDsGitHub <dingding8003@gmail.com>
2025-11-09 04:24:24 -0600

    changed db pool to download cert on runtime

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -1,21 +1,37 @@
-import pg from "pg";
+import { Pool } from "pg";
 import dotenv from "dotenv";
-import fs from "fs";
-import path from "path";
+import https from "https";
 
 dotenv.config();
 
-const { Pool } = pg;
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT) || 5432,
-  ssl: {
-    ca: fs.readFileSync(path.resolve(__dirname, "./us-east-1-bundle.pem")).toString(),
-    rejectUnauthorized: process.env.NODE_ENV === "production",
-  },
-});
+function getRdsCaBundle(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    https
+      .get(
+        "https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem",
+        (res) => {
+          let data = "";
+          res.on("data", (chunk) => (data += chunk));
+          res.on("end", () => resolve(data));
+        }
+      )
+      .on("error", reject);
+  });
+}
+
+export async function createPool() {
+  const ca = await getRdsCaBundle();
+
+  return new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT) || 5432,
+    ssl: { rejectUnauthorized: process.env.NODE_ENV === "production", ca },
+  });
+}
+
+const pool = await createPool();
 
 export default pool;
